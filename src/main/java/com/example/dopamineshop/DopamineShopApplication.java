33d99d62e761301f1bf636d3020f3f48d16725c6
Inna Kremleva <nnkrmlv@gmail.com>
2026-09-10 18:13:02 +0200

    added /health endpoint

---
+++ src/main/java/com/example/dopamineshop/DopamineShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.dopamineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopamineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopamineShopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/dopamineshop/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.dopamineshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/status")
+    public String getStatus() {
+        return "The status is OK.";
+    }
+}
